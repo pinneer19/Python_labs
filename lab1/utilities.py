f37f5a80fb77d82719e04a1c8a8885569d4847f7
pinneer19 <maxiqastflower21@gmail.com>
2023-02-27 23:02:45 +0300

    Even function was added

--- lab1/utilities.py
+++ lab1/utilities.py
@@ -4,8 +4,24 @@ def calculate(a, b, op):
         return "Error"
 
     division, multiply, addition, subtraction = "div", "mult", "add", "sub"
+    
     return a / b if op == division \
         else a * b if op == multiply \
         else a + b if op == addition \
         else a - b if op == subtraction \
         else "Error"
+        
+
+# Get even numbers from the given list
+def even(numbers_list):
+    if not all(isinstance(x, int) for x in numbers_list):
+        return "Error"
+
+    result = []
+
+    for i in numbers_list:
+        if i % 2 == 0:
+            result.append(i)
+
+    return result
+
