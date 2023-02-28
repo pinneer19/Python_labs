d5bff29c8a5f00f3e6c18c6479d062624b670d02
pinneer19 <maxiqastflower21@gmail.com>
2023-02-28 15:48:42 +0300

    Main file was added

---
+++ lab1/main.py
@@ -0,0 +1,22 @@
+from utilities import even
+from utilities import calculate
+
+
+def main():
+
+    # Task 1
+    print("Hello world!")
+
+    # Task 2
+    a = 10
+    b = 50
+    operation = "div"
+    print(calculate(a, b, operation))
+
+    # Task 3
+    numbers = [1, 2, 3, 7]
+    print(even(numbers))
+
+
+if __name__ == "__main__":
+    main()
