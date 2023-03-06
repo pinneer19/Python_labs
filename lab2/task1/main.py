50afcf848492ddffef8170e12f1db43227d4642d
pinneer19 <maxiqastflower21@gmail.com>
2023-03-06 13:04:30 +0300

    Calculate non-declarative sentences function was added

--- lab2/task1/main.py
+++ lab2/task1/main.py
@@ -1,9 +1,10 @@
-from utilities import amount_of_sentences
+from utilities import amount_of_sentences, amount_of_non_declarative_sentences
 
 
 def main():
     text = input('Enter your text: ')
     print('Amount of sentences in the text: ', amount_of_sentences(text))
+    print('Amount of non-declarative sentences in the text: ', amount_of_non_declarative_sentences(text))
 
 
 if __name__ == '__main__':
--- lab2/task1/utilities.py
+++ lab2/task1/utilities.py
@@ -3,3 +3,7 @@ import re
 
 def amount_of_sentences(text: str):
     return len(re.split(r"[.!?]+", text)) - 1
+
+
+def amount_of_non_declarative_sentences(text: str):
+    return len(re.split(r"[!?]+", text)) - 1
