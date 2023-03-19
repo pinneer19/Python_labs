ad30795cf101ed362827066e585d797f79406268
pinneer19 <maxiqastflower21@gmail.com>
2023-03-19 12:54:19 +0300

    Abbreviations support

---
+++ lab2/task1/constants.py
@@ -0,0 +1,16 @@
+ABBREVIATIONS = [
+    "A.M.", "P.M.", "Dr.", "Mr.", "Mrs.", "Ms.", "Jr.", "Sr.", "Co.", "Corp.", "Inc.", "Ltd.", "Gov.",
+    "Sen.", "Rep.", "Capt.", "Col.", "Gen.", "Sgt.", "Rev.", "Ph.D.", "M.D.", "M.A.", "B.S.",
+    "M.S.", "D.C.", "U.S.", "U.K.", "E.U.", "N.A.T.O.", "I.B.M.", "I.R.S.", "F.B.I.", "C.I.A.", "N.A.S.A.",
+    "A.D.", "B.C.", "U.F.O.", "S.O.S.", "E.T.", "I.Q.", "M.P.H.", "G.P.S.", "A.T.M.", "U.S.A.", "U.N.",
+    "A.I.", "A.V.", "C.E.O.", "M.B.A.", "C.F.O.", "C.T.O.", "B.T.W.", "E.T.A.", "I.O.U.", "N.B.", "R.S.V.P.",
+    "S.E.O.", "V.I.P.", "V.S.", "W.T.F.", "Y.O.L.O.", "P.S.", "P.P.S.", "e.g.", "i.e."
+]
+
+# 7 sentences
+SAMPLE_TEXT = "I just received my M.B.A. degree and now I'm ready to start my career in finance. I'm planning to " \
+              "apply for a job at a multinational corporation and hopefully, I'll be able to get a position in their " \
+              "corporate finance department. My goal is to become a C.F.O. someday and manage a team of financial " \
+              "analysts. I know it won't be easy, but I'm determined to work hard and learn as much as I can. I've " \
+              "already started networking with other professionals on LINKEDIN and attending finance seminars and " \
+              "workshops. I'm excited to see where my career takes me. Wish me luck!"
--- lab2/task1/utilities.py
+++ lab2/task1/utilities.py
@@ -1,9 +1,21 @@
 import re
+from constants import ABBREVIATIONS
 
 
 def amount_of_sentences(text: str):
-    return len(re.split(r"[.!?]+", text)) - 1
+    sentences = re.findall(r"[.!?]+", text)
+    amount = len(sentences)
+
+    for abbreviation in ABBREVIATIONS:
+        amount -= text.count(abbreviation) * abbreviation.count('.')
+
+    return amount
 
 
 def amount_of_non_declarative_sentences(text: str):
     return len(re.split(r"[!?]+", text)) - 1
+
+
+def average_sentence_length(text: str):
+    sentences_amount = amount_of_sentences(text)
+    return len(text.split()) / sentences_amount
